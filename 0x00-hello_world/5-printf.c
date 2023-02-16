82e2da507429bb97564a9a2bbe34fb9eef3da9a7
Dacodemaniac <u.emmanuel53@gmail.com>
2023-02-16 08:04:01 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A line of code that uses printf function
+ * Return: This returns 0 as (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
