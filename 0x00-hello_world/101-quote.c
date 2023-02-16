06fd8ee6f875971445626969110aaa17ab6d8aa1
Dacodemaniac <u.emmanuel53@gmail.com>
2023-02-16 12:09:26 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints exactly with new line
- * Return: This returns 1 (successful)
- */
+*main - A program that prints exactly
+*Return: returns 1 (successful)
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
