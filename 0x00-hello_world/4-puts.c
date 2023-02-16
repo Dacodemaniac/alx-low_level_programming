a3681be0b219df8584d2a6d3283836ec88e3d476
Dacodemaniac <u.emmanuel53@gmail.com>
2023-02-16 07:37:45 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - A program that prints a line of code using the puts function
+ * Return: shows 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
