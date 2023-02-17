8136236ee372d249f7a5d168e0ad025aab6ddd6b
Dacodemaniac <u.emmanuel53@gmail.com>
2023-02-17 14:21:07 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/*
+ * main - Returns all single units
+ * Description: prints all single digit
+ * Returns: return 0 (success)
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
